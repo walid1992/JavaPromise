60b01bebf7352857dd82d36e2905e260a2260faf
walid <541280314@qq.com>
2017-06-08 09:58:43 +0800

    格式化code

---
+++ app/src/main/java/com/walid/promise/main.java
@@ -0,0 +1,31 @@
+package com.walid.promise;
+
+import com.syswin.toon.appstub.event.promise.Promise;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class main {
+
+    public static void main(String[] args) {
+        Promise<String, Integer> promise = new Promise<>((resolve, reject) -> {
+            System.out.println("BEGIN");
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println("END");
+                    resolve.run("walid =>");
+                }
+            }, 2000);
+        });
+        promise.then(s -> {
+            System.out.println("then 1 : " + s);
+            return s + s;
+        }).then(s -> {
+            System.out.println("then 2 : " + s);
+            return s + s;
+        }).then(s -> {
+            System.out.println("then 3 : " + s);
+        }).fail(integer -> System.out.println("main fail"));
+    }
+}
